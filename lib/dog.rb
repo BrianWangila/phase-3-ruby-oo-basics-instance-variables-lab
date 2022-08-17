4ab2bb6db4fbab359d060889baecfbeee24e53c5
Brian Wangila <bwanjalah@gmail.com>
2022-08-17 20:28:41 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
+  def name=(dog_name)         #sets the name
+    @this_dogs_name = dog_name
+  end
 
+  def name      #gets the name
+    @this_dogs_name
+  end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
